8620098901fe696862b796bbed6748b33859f1f7
officialcycode <oluwasegunstar@gmail.com>
2021-09-07 23:50:47 +0100

    Fix build errors

--- src/components/layout/Header/TopHeader.js
+++ src/components/layout/Header/TopHeader.js
@@ -2,13 +2,13 @@ import '../../../../styles/Header.module.scss'
 
 const TopHeader = () => {
     return (
-        <div className="sm-text-center top-header">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-sm-8">
+        <div className={`sm-text-center top-header`}>
+            <div className={`container-fluid`}>
+                <div className={`row`}>
+                    <div className={`col-sm-8`}>
                     Test
                     </div>
-                     <div className="col-sm-4">
+                     <div className={`col-sm-4`}>
                     </div>
                 </div>
             </div>
