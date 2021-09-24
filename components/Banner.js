b7bce38ba421b24e7f6cf14e26b82b88a6c56e94
AndySakov <andysakov1958@gmail.com>
2021-09-24 12:55:43 +0100

    fixed banner background image issue

--- components/Banner.js
+++ components/Banner.js
@@ -1,26 +1,23 @@
 import React from 'react'
 import SearchBar from './SearchBar'
-import styled from 'styled-components'
 
 const Banner = ({ search = false, background, children }) => {
 
-    const BannerDiv = styled.div`
-        background-image: url('${background.src}');
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-        background-size: cover;
-        padding: 0;
-        margin: 0;
-        width: 100%;
-    `
+    // background-repeat: no-repeat;
+    // background-attachment: fixed;
+    // background-size: cover;
 
     return (
-        <BannerDiv>
+        <div style={{ 
+            backgroundImage: `url(${background.src})`,
+            backgroundAttachment: 'fixed',
+            backgroundSize: 'cover'
+        }}>
             <div className="banner-inner">
                 {children}
                 <SearchBar show={search ? 'true' : 'false'} />
             </div>
-        </BannerDiv>
+        </div>
     )
 }
 
