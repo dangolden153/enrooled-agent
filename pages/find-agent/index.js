1078513133732b21e4e6b6dc3a391184abfbd834
AndySakov <andysakov1958@gmail.com>
2021-09-28 18:19:53 +0100

    implemented find agent page

--- pages/find-agent/index.js
+++ pages/find-agent/index.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import banner1 from '../../public/images/banner1.png'
+import Banner from "../../components/Banner";
+import TopRated from '../../components/TopRated';
+import SearchSection from '../../components/SearchSection';
+import MostRequested from '../../components/MostRequested';
 
 const index = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Banner search background={banner1}>
+                <h1 className="banner-title">Find Your Next IRS Enrolled Agent</h1>
+                <h6 className="banner-extra">
+                    Browse through our directory of over 60,000 professionals and engage
+                    them for free
+                </h6>
+            </Banner>
+
+            <SearchSection />
+
+            <MostRequested />
+
+            <TopRated />
+        </>
     )
 }
 
