c22947fe0d6577fd869dd3c16c605e45b4014612
AndySakov <andysakov1958@gmail.com>
2021-10-04 10:31:42 +0100

    made it responsive

--- pages/blog/all/index.js
+++ pages/blog/all/index.js
@@ -6,19 +6,18 @@ import styles from '../../../styles/blog/NewsAndArticles.module.scss'
 const index = () => {
     return (
         <div className={`${styles.newsAndArticles}`}>
-            <div className={`${styles.newsHeader} d-flex flex-column`}>
+            <div className={`${styles.header} d-flex flex-column`}>
                 <h1>News & Articles</h1>
+                <hr />
+                <h4>All News</h4>
             </div>
-
-            <hr />
-
-            <h4>All News</h4>
 
             <News />
 
-            <hr />
-
-            <h4>All Articles</h4>
+            <div className={`${styles.header} d-flex flex-column`}>
+                <hr />
+                <h4>All Articles</h4>
+            </div>
 
             <Articles />
 
