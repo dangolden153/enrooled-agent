05d34335029942ae756112ea602e09278d2620cc
Emmanuel Onuiteshi <emmanuelonuiteshi@gmail.com>
2021-10-05 12:12:47 +0100

    recent-article

--- pages/dashboard/articles/index.js
+++ pages/dashboard/articles/index.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Dollar from "../../../public/images/dollar-bill.png";
 
 const index = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="container recent-article">
+      <div className="article-head">
+        <h1>News & Articles</h1>
+      </div>
 
-export default index
+      <hr />
+
+      <h4>Recent News</h4>
+
+      <div className="col-md-6 col xl-6">
+        <div className="card mb-3">
+          <Image src={Dollar} />
+          <div className="card-body">
+            <p className="card-text">
+              <small class="text-muted">24th September, 2021</small>
+            </p>
+            <h5 className="card-title">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.{" "}
+            </h5>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-6 col xl-6"></div>
+    </div>
+  );
+};
+
+export default index;
