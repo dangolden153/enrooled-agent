55f89b15e04f70aca06bb2737da927440c53a424
officialcycode <oluwasegunstar@gmail.com>
2021-10-05 19:01:00 +0100

    added links to footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,58 +1,90 @@
-import React from 'react'
-import Image from 'next/image'
-import logoImage from '../../public/logo2.png'
-import { Facebook, Twitter, Linkedin, Instagram } from 'react-bootstrap-icons'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import logoImage from "../../public/logo2.png";
+import { Facebook, Twitter, Linkedin, Instagram } from "react-bootstrap-icons";
 
 const Footer = () => {
-    return (
-        <footer className='footer container-fluid'>
-            <div className="footer-inner">
-                <div className="col-12">
-                    <div className="row footer-up d-flex justify-content-between">
-                        <div className="col-6">
-                            <Image src={logoImage} alt="pixs" width="267" height="40" className="" />
-                        </div>
-                        <div className="d-flex col-6 justify-content-end">
-                            <button className='btn btn-outline'>Sign Up</button>
-                            <button className='btn btn-outline'>Login</button>
-                        </div>
-                    </div>
-                    <hr />
-                    <div className="row footer-down d-flex justify-content-between">
-                        <div className="col-lg-2 socials">
-                            <div className="d-flex flex-row justify-content-between">
-                                <Facebook size='25' />
-                                <Twitter size='25' />
-                                <Linkedin size='25' />
-                                <Instagram size='25' />
-                            </div>
-                        </div>
-                        <div className="col-lg-8 d-flex flex-row justify-content-center">
-                            <div className="col-6">
-                                <ul className="footer-link-list">
-                                    <li className="footer-link">Find a Tax Expert in your locality</li>
-                                    <li className="footer-link">All Enrolled Agents</li>
-                                    <li className="footer-link">Ask an EA</li>
-                                    <li className="footer-link">Verify an Enrolled Agents License</li>
-                                    <li className="footer-link">Enrolled Agent</li>
-                                    <li className="footer-link">Free Enrolled Agent Listings</li>
-                                </ul>
-                            </div>
-                            <div className="col-">
-                                <ul className="footer-link-list">
-                                    <li className="footer-link">About Us</li>
-                                    <li className="footer-link"> Contact Us</li>
-                                    <li className="footer-link">Privacy Policy</li>
-                                    <li className="footer-link">Terms of Use</li>
-                                    <li className="footer-link">FAQs</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <footer className="footer container-fluid">
+      <div className="footer-inner">
+        <div className="col-12">
+          <div className="row footer-up d-flex justify-content-between">
+            <div className="col-6">
+              <Image
+                src={logoImage}
+                alt="pixs"
+                width="267"
+                height="40"
+                className=""
+              />
             </div>
-        </footer>
-    )
-}
+            <div className="d-flex col-6 justify-content-end">
+              <button className="btn btn-outline">Sign Up</button>
+              <button className="btn btn-outline">Login</button>
+            </div>
+          </div>
+          <hr />
+          <div className="row footer-down d-flex justify-content-between">
+            <div className="col-lg-2 socials">
+              <div className="d-flex flex-row justify-content-between">
+                <Facebook size="25" />
+                <Twitter size="25" />
+                <Linkedin size="25" />
+                <Instagram size="25" />
+              </div>
+            </div>
+            <div className="col-lg-8 d-flex flex-row justify-content-center">
+              <div className="col-6">
+                <ul className="footer-link-list">
+                  <li className="footer-link">
+                    <Link href="/find-agent">
+                      Find a Tax Expert in your locality
+                    </Link>
+                  </li>
+                  <li className="footer-link">
+                    <Link href="/ea-listings">All Enrolled Agents</Link>
+                  </li>
+                  <li className="footer-link">
+                    <Link href="/ask-an-ea">Ask an EA</Link>
+                  </li>
+                  <li className="footer-link">
+                    <Link href="/license-verification">
+                      Verify an Enrolled Agents License
+                    </Link>
+                  </li>
+                  <li className="footer-link">
+                    <Link href="/new-listing">
+                      Free Enrolled Agent Listings
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-">
+                <ul className="footer-link-list">
+                  <li className="footer-link">
+                    <Link href="/about">About Us</Link>
+                  </li>
+                  <li className="footer-link">
+                    <Link href="/contact"> Contact Us</Link>
+                  </li>
+                  <li className="footer-link">
+                    <Link href="/privacy-policy">Privacy Policy</Link>
+                  </li>
+                  <li className="footer-link">
+                    <Link href="/terms">Terms of Use</Link>
+                  </li>
+                  <li className="footer-link">
+                    <Link href="/faq">FAQs</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
