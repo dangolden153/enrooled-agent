23bd9d5c1462b9ded67706e0b72341a88fac3136
Emmanuel Onuiteshi <emmanuelonuiteshi@gmail.com>
2021-10-14 14:58:49 +0100

    new changes

--- pages/new-listing/index.js
+++ pages/new-listing/index.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import Banner from "../../components/Banner";
+import NewListing from "../../public/images/banners/cropped-view.png";
 
 const index = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <Banner background={NewListing}>
+        <div className="bannerText">
+          <h1>Apply For New Listing</h1>
         </div>
-    )
-}
+      </Banner>
+      <div className="new-listing container">
+        <div>
+          <h3>Personal</h3>
+          <div className="row"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default index
+export default index;
