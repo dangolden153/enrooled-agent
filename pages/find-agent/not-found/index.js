6e4c1a658e3cf13e7f4b711febfd5efd160762f9
officialcycode <oluwasegunstar@gmail.com>
2021-10-18 22:40:46 +0100

    removed image import causing error

--- pages/find-agent/not-found/index.js
+++ pages/find-agent/not-found/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import banner1 from "../../../public/images/search.png";
+// import banner1 from "../../../public/images/search.png";
 import Banner from "../../../components/Banner";
 const index = () => {
     return (
         <div>
-            <Banner search background={banner1}></Banner>
+            <Banner search background=""></Banner>
             <div className="d-flex flex-row justify-content-center">
                 <h2 className="mt-5" style={{ marginBottom: '30rem' }}>Sorry, we couldn't find any results for "David"</h2>
             </div>
