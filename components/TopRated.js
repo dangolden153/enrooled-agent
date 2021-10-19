02d52bc29342f1b8685e57fc6f94a12fbf2932e0
Austin <augustineumeagudosi@gmail.com>
2021-10-19 19:57:32 +0100

    fixed merge issue

--- components/TopRated.js
+++ components/TopRated.js
@@ -1,40 +1,43 @@
-import React,{useEffect, useState} from 'react'
-import { useSelector} from 'react-redux';
-import TopRatedCard from './TopRatedCard';
-import { ChevronRight } from 'react-bootstrap-icons';
-import woman from '../public/images/card-woman.png'
-import man from '../public/images/card-man.png'
-import avatar from '../public/images/avatar.png'
-import CardList from './CardList';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import TopRatedCard from "./TopRatedCard";
+import { ChevronRight } from "react-bootstrap-icons";
+import woman from "../public/images/card-woman.png";
+import man from "../public/images/card-man.png";
+import avatar from "../public/images/avatar.png";
+import CardList from "./CardList";
 
 const TopRated = () => {
-    const agents = useSelector((state) => state.getAgents.topAgents);
-    console.log(agents);
-    return (
-        <div className="top-rated d-flex flex-column w-100 align-items-center justify-content-center">
-            <div className="col-12 d-flex flex-column align-items-center justify-content-center">
-                <h1>Top Rated Enrolled Agents</h1>
-                <span>Licensed and Verified Enrolled Agents</span>
-            </div>
+  const agents = useSelector((state) => state.getAgents.topAgents);
+  console.log(agents);
+  return (
+    <div className="top-rated d-flex flex-column w-100 align-items-center justify-content-center">
+      <div className="col-12 d-flex flex-column align-items-center justify-content-center">
+        <h1>Top Rated Enrolled Agents</h1>
+        <span>Licensed and Verified Enrolled Agents</span>
+      </div>
 
-            <CardList>
-                {agents && agents.map((agent, index) => {
-                return (
-                <TopRatedCard
-                    key={index}
-                    image={agent.agent && (agent.agent.image_url ?? avatar)}
-                    name={agent.agent && agent.agent.first_name + agent.agent.first_name}
-                    location={agent.agent && agent.agent.city + ',' + agent.agent.state}
-                    numStars={agent.rating}
-                    profileLink={`agent/${agent.id}/${agent.first_name}`}
-                />
-                    )
-                })}
-            </CardList>
+      <CardList>
+        {agents &&
+          agents.map((agent, index) => {
+            return (
+              <TopRatedCard
+                key={index}
+                image={agent.agent && (agent.agent.image_url ?? avatar)}
+                name={agent.agent && agent.agent.first_name + agent.agent.first_name}
+                location={agent.agent && agent.agent.city + "," + agent.agent.state}
+                numStars={agent.rating}
+                profileLink={`agent/${agent.id}/${agent.first_name}-${agent.last_name}`}
+              />
+            );
+          })}
+      </CardList>
 
-            <button type="button" className='btn-common'>Find more local EA's <ChevronRight /></button>
-        </div>
-    )
-}
+      <button type="button" className="btn-common">
+        Find more local EA's <ChevronRight />
+      </button>
+    </div>
+  );
+};
 
 export default TopRated;
--- pages/agent/[id]/[name].js
+++ pages/agent/[id]/[name].js
@@ -1,17 +1,18 @@
-import React,{useState} from "react";
-import {useSelector, shallowEqual} from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useRouter } from "next/router";
 import Image from "next/image";
-import Smilie from "../../../public/images/PP.png";
+import Link from "next/link";
 import Head from "next/head";
-// import { Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { getSingleAgent } from "../../../src/redux/actions/agent";
+import avatar from "../../../public/images/avatar.png";
 import {
   fab,
   faFacebook,
   faInstagram,
-  faLinkedin, 
-  //   faUser,
+  faLinkedin,
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 import styles from "../../../styles/agents/viewagent.module.scss";
@@ -23,204 +24,185 @@ library.add(fab, faInstagram);
 // library.add(far, faUser);
 
 const index = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const agentData = useSelector((state) => state.getAgents.singleAgent);
+  const [isLoading, setLoading] = useState(true);
+
+  const handleGetAgent = async () => {
+    const res = await dispatch(getSingleAgent(router.query));
+    if (res) setLoading(false);
+  };
+
+  useEffect(() => {
+    if (router.isReady) handleGetAgent();
+  }, [router]);
+
+  if (isLoading) {
+    return <p>Loading</p>;
+  }
 
   return (
     <>
-        <Head>
-            <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-            <meta httpEquiv="content-type" content="text/html; charset=UTF-8" />
-            <meta charset="UTF-8" />
-            <meta name="google" content="notranslate" />
-            <meta httpEquiv="Content-Language" content="en" />
-            <meta name="description" 
-            content="Find a tax professional for your tax requirements. 
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <meta httpEquiv="content-type" content="text/html; charset=UTF-8" />
+        <meta charset="UTF-8" />
+        <meta name="google" content="notranslate" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta
+          name="description"
+          content="Find a tax professional for your tax requirements. 
              Enrolled Agent has a list of verified tax practitioners. 
-             Search from the lists of irs enrolled agent ." />
-            <meta name="keywords"
-                content="consulting,finance,advisor,consultant,business,service,insurance,agency" />
-            <meta name="Enrolled Agent" content="Enrolled Agent" />
+             Search from the lists of irs enrolled agent ."
+        />
+        <meta
+          name="keywords"
+          content="consulting,finance,advisor,consultant,business,service,insurance,agency"
+        />
+        <meta name="Enrolled Agent" content="Enrolled Agent" />
 
-            <title>Enrolled Agent Lookup | Find Tax Preparer |  IRS Enrolled Agent Search</title>
-        </Head>
-    <div className={styles.viewAgent}>
-      <div className={styles.top}>
-        {/* <Container> */}
-        <div className={styles.left}>
-          <div>
-            <Image src={Smilie} alt="An icon of a lady" />
-          </div>
-          <div className={styles.info}>
-            <p className={styles.personal}>
-              <b>Personal Information</b>
-            </p>
-            <hr />
-            <div className={styles.contactDetails}>
-              <p id={styles.phone}>
-                <b>Address: </b>
-              </p>
-              <p id={styles.phone2}>EVERGREEN, NC 28438</p>
-            </div>
-            <div className={styles.contactDetails}>
-              <p id={styles.phone}>
-                <b>Phone number: </b>
-              </p>
-              <p id={styles.phone2}>404-862-9929</p>
-            </div>
-
-            <div className={styles.socialIcons}>
-              <FontAwesomeIcon icon={["fab", "facebook-f"]} id={styles.icon} />
-              <FontAwesomeIcon icon={["fab", "twitter"]} id={styles.icon} />
-              <FontAwesomeIcon icon={["fab", "linkedin"]} id={styles.icon} />
-              <FontAwesomeIcon icon={["fab", "instagram"]} id={styles.icon} />
-            </div>
-
-            <div className={styles.buttons}>
-              <div className={styles.butt}>
-                <button>Call</button>
-              </div>
-              <div className={styles.buton}>
-                <button>Message</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* </Container> */}
-        <div className={styles.rightDiv}>
-          <div className={styles.right}>
-            <div className={styles.topleft}>
-              <div className={styles.claim}>
-                <h3 id={styles.agent_name}>Jane Doe</h3>
-                <p id={styles.agent_status}>(Claimed)</p>
-              </div>
-
-              <div className={styles.btn}>
-                <button>
-                  Write a review
-                </button>
-              </div>
-            </div>
-            <p className={styles.expert}>
-              <b>Tax Professional</b>
-            </p>
+        <title>Enrolled Agent Lookup | Find Tax Preparer | IRS Enrolled Agent Search</title>
+      </Head>
+      <div className={styles.viewAgent}>
+        <div className={styles.top}>
+          {/* <Container> */}
+          <div className={styles.left}>
             <div>
-              <p id={styles.rank}>Ratings</p>
-              <p className={styles.star}>
-                8.7 <span className={styles.stars}>&#9733;</span>{" "}
-                <span className={styles.stars}>&#9733;</span>
-                <span className={styles.stars}>&#9733;</span>{" "}
-                <span className={styles.stars}>&#9733;</span>
+              <Image src={agentData?.image_url || avatar} alt="An icon of a lady" />
+            </div>
+            <div className={styles.info}>
+              <p className={styles.personal}>
+                <b>Personal Information</b>
               </p>
+              <hr />
+              {agentData?.showAddress && (
+                <div className={styles.contactDetails}>
+                  <p id={styles.phone}>
+                    <b>Address: </b>
+                  </p>
+                  <p id={styles.phone2}>{agentData?.address1}</p>
+                </div>
+              )}
+              <div className={styles.contactDetails}>
+                <p id={styles.phone}>
+                  <b>Phone number: </b>
+                </p>
+                <p id={styles.phone2}>{agentData?.phone}</p>
+              </div>
+
+              <div className={styles.socialIcons}>
+                <Link href={agentData?.facebook || "#"}>
+                  <FontAwesomeIcon icon={["fab", "facebook-f"]} id={styles.icon} />
+                </Link>
+                <Link href={agentData?.twitter || "#"}>
+                  <FontAwesomeIcon icon={["fab", "twitter"]} id={styles.icon} />
+                </Link>
+                <Link href={agentData?.linkedIn || "#"}>
+                  <FontAwesomeIcon icon={["fab", "linkedin"]} id={styles.icon} />
+                </Link>
+                <Link href={agentData?.instagram || "#"}>
+                  <FontAwesomeIcon icon={["fab", "instagram"]} id={styles.icon} />
+                </Link>
+              </div>
+
+              <div className={styles.buttons}>
+                <div className={styles.butt}>
+                  <Link href={agentData?.phone ? `tel:${agentData?.phone}` : "#"}>
+                    <button>Call</button>
+                  </Link>
+                </div>
+                {agentData?.allow_contact_message && (
+                  <div className={styles.buton}>
+                    <button>Message</button>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
-          {/* <div>
+          {/* </Container> */}
+          <div className={styles.rightDiv}>
+            <div className={styles.right}>
+              <div className={styles.topleft}>
+                <div className={styles.claim}>
+                  <h3 id={styles.agent_name}>
+                    {agentData?.first_name} {agentData?.last_name}
+                  </h3>
+                  {agentData?.isClaimed && <p id={styles.agent_status}>(Claimed)</p>}
+                </div>
+
+                <div className={styles.btn}>
+                  <button>Write a review</button>
+                </div>
+              </div>
+              <p className={styles.expert}>
+                <b>Tax Professional</b>
+              </p>
+              <div>
+                <p id={styles.rank}>Ratings</p>
+                <p className={styles.star}>
+                  8.7 <span className={styles.stars}>&#9733;</span>{" "}
+                  <span className={styles.stars}>&#9733;</span>
+                  <span className={styles.stars}>&#9733;</span>{" "}
+                  <span className={styles.stars}>&#9733;</span>
+                </p>
+              </div>
+            </div>
+            {/* <div>
 <p>what is going on here?</p>
       </div> */}
-          <div className={styles.right}>
-            <div className={styles.detail}>
-              <p id={styles.detailA}>
-                <u>About</u>
-              </p>
-              <p id={styles.detailB}>Enrolled Agent Details</p>
-            </div>
-            <div id={styles.line}>
-              <hr />
+            <div className={styles.right}>
+              <div className={styles.detail}>
+                <p id={styles.detailA}>
+                  <u>About</u>
+                </p>
+                <p id={styles.detailB}>Enrolled Agent Details</p>
+              </div>
+              <div id={styles.line}>
+                <hr />
+              </div>
+
+              <div className={styles.enroledDetail}>
+                <p>{agentData?.bio}</p>
+              </div>
             </div>
 
-            <div className={styles.enroledDetail}>
-              <p>
-                Jane Doe is an Enrolled Agent who operates in and around
-                EVERGREEN. Jane Doe is admitted to practice unrestricted before
-                the Internal Revenue Service and can represent clients
-                throughout the United States. Jane Doe is an EnrolledAgent.com
-                verified Enrolled Agent. Enrolled Agent status is the highest
-                credential awarded by the Internal Revenue Service. Jane Doe is
-                Enrolled Agent credential is recognized across all 50 US states.
-              </p>
-            </div>
-          </div>
-
-          <div className={styles.bottomDiv}>
-            <div className={styles.client}>
-              <p id={styles.reviewed}>
-                <u>Client Reviews</u>
-              </p>
-              <p>
-                Sort by: <b>Rating</b>
-              </p>
-            </div>
-            <div className={styles.word}>
-              <div>
-                <p>Nyla Word</p>
-                <p className={styles.rate}>
-                  <span className={styles.stars}>&#9733;</span>{" "}
-                  <span className={styles.stars}>&#9733;</span>{" "}
-                  <span className={styles.stars}>&#9733;</span>
-                  <span className={styles.stars}>&#9733;</span>
+            <div className={styles.bottomDiv}>
+              <div className={styles.client}>
+                <p id={styles.reviewed}>
+                  <u>Client Reviews</u>
+                </p>
+                <p>
+                  Sort by: <b>Rating</b>
                 </p>
               </div>
-              <p id={styles.dates}>30 October 2021</p>
-            </div>
-            <p className={styles.p3}>
-              Jane is very professional, meticulous and pateint in getting my
-              outstanding tax issues resolved. She helped put me at ease and did
-              a all the heavy lifting in a very overwhelming case for me. She
-              pays attention to details and will work tireless to get the best
-              out of any situation.
-            </p>
-            <hr className={styles.border} />
-
-            <div className={styles.word}>
-              <div>
-                <p>Ryan Richards</p>
-                <p className={styles.rate}>
-                  <span className={styles.stars}>&#9733;</span>{" "}
-                  <span className={styles.stars}>&#9733;</span>{" "}
-                  <span className={styles.stars}>&#9733;</span>
-                  <span className={styles.stars}>&#9733;</span>
-                </p>
+              <div className={styles.word}>
+                <div>
+                  <p>Nyla Word</p>
+                  <p className={styles.rate}>
+                    <span className={styles.stars}>&#9733;</span>{" "}
+                    <span className={styles.stars}>&#9733;</span>{" "}
+                    <span className={styles.stars}>&#9733;</span>
+                    <span className={styles.stars}>&#9733;</span>
+                  </p>
+                </div>
+                <p id={styles.dates}>30 October 2021</p>
               </div>
-              <p id={styles.dates}>29 October 2021</p>
-            </div>
-            <p className={styles.p3}>
-              Jane is very professional, meticulous and pateint in getting my
-              outstanding tax issues resolved. She helped put me at ease and did
-              a all the heavy lifting in a very overwhelming case for me. She
-              pays attention to details and will work tireless to get the best
-              out of any situation.
-            </p>
-            <hr className={styles.border} />
+              <p className={styles.p3}>
+                Jane is very professional, meticulous and pateint in getting my outstanding tax
+                issues resolved. She helped put me at ease and did a all the heavy lifting in a very
+                overwhelming case for me. She pays attention to details and will work tireless to
+                get the best out of any situation.
+              </p>
+              <hr className={styles.border} />
 
-            {/* <div className={styles.client}> */}
-            {/* <p><u>Client Reviews</u></p> */}
-            {/* <p>Sort by: <b>Rating</b></p> */}
-            {/* </div> */}
-            <div className={styles.word}>
-              <div>
-                <p>Nyla Word</p>
-                <p className={styles.rate}>
-                  <span className={styles.stars}>&#9733;</span>{" "}
-                  <span className={styles.stars}>&#9733;</span>{" "}
-                  <span className={styles.stars}>&#9733;</span>
-                  <span className={styles.stars}>&#9733;</span>
-                </p>
+              <div className={styles.seeAll}>
+                <p>See all reviews</p>
               </div>
-              <p id={styles.dates}>25 October 2021</p>
-            </div>
-            <p className={styles.p3}>
-              Jane is very professional, meticulous and pateint in getting my
-              outstanding tax issues resolved. She helped put me at ease and did
-              a all the heavy lifting in a very overwhelming case for me. She
-              pays attention to details and will work tireless to get the best
-              out of any situation.
-            </p>
-            <hr className={styles.border} />
-            <div className={styles.seeAll}>
-              <p>See all reviews</p>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
