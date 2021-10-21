d8e8142996778aa5d7776292fd963d9cb7b1502d
officialcycode <oluwasegunstar@gmail.com>
2021-10-21 16:24:52 +0100

    fixed build errors

--- pages/auth/reset-password/reset.js
+++ pages/auth/reset-password/reset.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from "../../styles/auth/Reset.module.scss"
+import styles from "../../../styles/auth/Reset.module.scss"
 const reset = () => {
     return (
       <div>
