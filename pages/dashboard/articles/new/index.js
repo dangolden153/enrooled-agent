93fd542657923340fb744e0332340442e85a603a
Tope Olotu <topeolotu75@gmail.com>
2021-10-25 22:39:35 +0100

    latest updates@

--- pages/dashboard/articles/new/index.js
+++ pages/dashboard/articles/new/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../../../../styles/Articles/NewArticle.module.scss";
-
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 const index = () => {
   return (
     <div className={styles.new}>
