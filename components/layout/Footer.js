4697ac51719456c623a19883caf4e69169efaa67
Emmanuel Onuiteshi <emmanuelonuiteshi@gmail.com>
2021-10-28 09:06:47 +0100

    faq links to home

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -91,7 +91,7 @@ const Footer = () => {
                     <Link href="/terms">Terms of Use</Link>
                   </li>
                   <li className="footer-link">
-                    <Link href="/faq">FAQs</Link>
+                    <Link href="/">FAQs</Link>
                   </li>
                 </ul>
               </div>
